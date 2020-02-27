030e08e228d5b563e0fb081f7edfc8ae7d977131
Martin Goldhahn <mgoldhahn@gmail.com>
2020-02-27 17:42:17 +0100

    kubernetes host from data

--- cloud/aws/k8s/kubernetes.tf
+++ cloud/aws/k8s/kubernetes.tf
@@ -1,5 +1,5 @@
 provider "kubernetes" {
-  host = aws_eks_cluster.plantuml.endpoint
+  host = data.aws_eks_cluster.plantuml.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.plantuml.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.plantuml.token
   load_config_file       = false
